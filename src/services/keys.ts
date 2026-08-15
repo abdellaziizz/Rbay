cd1a06a3cf7d8977e67baf1384c3983804377eec
ahmed abdelaziz <a.abdelaziz2338@gmail.com>
2026-08-15 14:59:45 +0300

    createItem

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,2 +1,6 @@
+import { DateTime } from 'luxon';
+
 export const pageCacheKey = (route: string) => `pageCache#${route}`;
 export const userKey = (userId: string) => `users${userId}`;
+export const sessionKey = (sessionId: string) => `sesssion#${sessionId}`;
+export const itemKey = (itemId: string) => `item#${itemId}`;
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,14 @@
 import type { CreateItemAttrs } from '$services/types';
-
+import { serialize } from '$services/queries/items/serialize';
+import { client } from '$services/redis';
+import { genId } from '$services/utils';
+import { itemKey } from '$services/keys';
 export const getItem = async (id: string) => {};
 
 export const getItems = async (ids: string[]) => {};
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+export const createItem = async (attrs: CreateItemAttrs) => {
+	const id = genId();
+	const serialized = serialize(attrs);
+	await client.hSet(itemKey(id), serialized);
+};
