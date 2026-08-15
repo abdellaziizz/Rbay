cda303ac143f0b42e7bbe3d9a6b6d63aa90212ce
ahmed abdelaziz <a.abdelaziz2338@gmail.com>
2026-08-16 00:49:27 +0300

    getItem, getItems using the pipeline

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,13 +2,20 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
-		color: 'red',
-		model: 1999,
-		enigne: { cylinder: 8 },
-		owner: '',
-		serivce: ''
-	}); //HSET car color red model 1999
+	const result = await Promise.all([
+		client.hSet('car1', {
+			color: 'red',
+			model: 1999
+		}),
+		client.hSet('car2', {
+			color: 'green',
+			model: 2000
+		}),
+		client.hSet('car3', {
+			color: 'blue',
+			model: 2001
+		})
+	]);
 };
 run();
 const car = await client.hGetAll('car');
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -3,9 +3,23 @@ import { serialize } from '$services/queries/items/serialize';
 import { client } from '$services/redis';
 import { genId } from '$services/utils';
 import { itemKey } from '$services/keys';
-export const getItem = async (id: string) => {};
+import { deserialize } from './deserialize';
+export const getItem = async (id: string) => {
+	const item = await client.hGetAll(itemKey(id));
+	if (Object.keys(item).length === 0) return;
+	return deserialize(id, item);
+};
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]) => {
+	const commands = ids.map((id) => {
+		return client.hGetAll(itemKey(id));
+	});
+	const results = await Promise.all(commands);
+	return results.map((result, i) => {
+		if (Object.keys(result).length === 0) return null;
+		return deserialize(ids[i], result);
+	});
+};
 
 export const createItem = async (attrs: CreateItemAttrs) => {
 	const id = genId();
