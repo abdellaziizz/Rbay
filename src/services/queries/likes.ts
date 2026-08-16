dd86761aa949f0408cd550dbac06900e25803e75
ahmed abdelaziz <a.abdelaziz2338@gmail.com>
2026-08-17 00:05:51 +0300

    common likes between two users

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,10 +1,17 @@
 import { client } from '$services/redis';
 import { itemKey, userLikesKey } from '$services/keys';
+import { getItems } from './items';
 export const userLikesItem = async (itemId: string, userId: string) => {
 	return client.SISMEMBER(userLikesKey(userId), itemId);
 };
 
-export const likedItems = async (userId: string) => {};
+export const likedItems = async (userId: string) => {
+	//1.fetch all the item ids from the users liked set
+	const ids = await client.sMembers(userLikesKey(userId));
+
+	//2.fetch all the item hases with thoses ids "returned as array"
+	return getItems(ids);
+};
 
 export const likeItem = async (itemId: string, userId: string) => {
 	const inserted = await client.sAdd(userLikesKey(userId), itemId);
@@ -20,4 +27,7 @@ export const unlikeItem = async (itemId: string, userId: string) => {
 	}
 };
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+	const ids = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)]);
+	return getItems(ids);
+};
