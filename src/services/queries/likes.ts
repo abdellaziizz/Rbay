82cc379dc6445bf5998a77be69b65d651d9c05ae
ahmed abdelaziz <a.abdelaziz2338@gmail.com>
2026-08-16 21:40:57 +0300

    adding/removing likes p2

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,15 +1,23 @@
 import { client } from '$services/redis';
-import { userLikesKey } from '$services/keys';
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { itemKey, userLikesKey } from '$services/keys';
+export const userLikesItem = async (itemId: string, userId: string) => {
+	return client.SISMEMBER(userLikesKey(userId), itemId);
+};
 
 export const likedItems = async (userId: string) => {};
 
 export const likeItem = async (itemId: string, userId: string) => {
-	await client.sAdd(userLikesKey(userId), itemId);
+	const inserted = await client.sAdd(userLikesKey(userId), itemId);
+	if (inserted) {
+		await client.hIncrBy(itemKey(itemId), 'likes', 1);
+	}
 };
 
 export const unlikeItem = async (itemId: string, userId: string) => {
-	await client.sRem(userLikesKey(userId), itemId);
+	const removed = await client.sRem(userLikesKey(userId), itemId);
+	if (removed) {
+		await client.hIncrBy(itemKey(itemId), 'likes', -1);
+	}
 };
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
