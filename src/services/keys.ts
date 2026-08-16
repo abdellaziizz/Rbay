8fc9e44d94132bd14a79aeff155096d46ccdb56d
ahmed abdelaziz <a.abdelaziz2338@gmail.com>
2026-08-16 21:24:24 +0300

    like / unlike item p1

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -4,3 +4,5 @@ export const pageCacheKey = (route: string) => `pageCache#${route}`;
 export const userKey = (userId: string) => `users${userId}`;
 export const sessionKey = (sessionId: string) => `sesssion#${sessionId}`;
 export const itemKey = (itemId: string) => `item#${itemId}`;
+export const usernamesUniqueKey = () => `usernames#unique`;
+export const userLikesKey = (userId: string) => `users:likes#${userId}`;
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,7 +1,7 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { userKey } from '$services/keys';
+import { userKey, usernamesUniqueKey } from '$services/keys';
 import { attr } from 'svelte/internal';
 // import { deserialize } from 'v8';
 
@@ -13,8 +13,15 @@ export const getUserById = async (id: string) => {
 };
 
 export const createUser = async (attrs: CreateUserAttrs) => {
+	//check if the username exist or not in the set of usernames. If exist, throw error. Else, continue
+	const isExist = await client.SISMEMBER(usernamesUniqueKey(), attrs.username);
+	if (isExist) {
+		throw new Error('UserName is already exist');
+	}
+
 	const id = genId();
 	await client.hSet(userKey(id), serialize(attrs));
+	await client.sAdd(usernamesUniqueKey(), attrs.username);
 	return id;
 };
 const serialize = (user: CreateUserAttrs) => {
